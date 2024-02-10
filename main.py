8fa3b5602db2ed748546f31b84a4afe1653ab318
Vladimir Viazovetskov <erhosen@gmail.com>
2024-02-10 13:14:31 +0100

    Outdated todos.

--- main.py
+++ main.py
@@ -34,10 +34,7 @@ if __name__ == "__main__":
     # profiler.print(show_all=False)
 
 
-# TODO: Сохранять всегда оригиналы, чтобы можно было писать тесты
-# TODO: Если что-то идет не так, например неожиданный экран, то сделать скриншот и сохранить в screen/unexpected.png
 # TODO: Таймер, который знает сколько секунд длится каждый уровень, и если прошло больше, чем должно было, то выходит на TimeoutScreen
 # TODO: Rich, чтобы было красиво
 # TODO: Typer, чтобы было удобно
-# TODO: IntermediateScreen / ExtremeScreen collision
-# TODO: Make sure it works fine on different screens / resolutions
+# TODO: Screen collisions in general: worth to consider?
